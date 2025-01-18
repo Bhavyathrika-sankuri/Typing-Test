44986090680f4ef1c82d31f694268315dfa03cd8
Bhavyathrika <Bhavyathrika@gmail.com>
2025-01-18 16:21:22 +0530

    updated temp conversion

--- tyt.js
+++ tyt.js
@@ -2,27 +2,35 @@
 document.addEventListener("DOMContentLoaded" , () => {
 
     const arr = [1,2,3,4,5,6,7,8,9,0,"q","W","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","c","v","b","n","m","<",">","?","!",":","{","}"];
-    let card = document.querySelector("#card");
+    let card = document.querySelector("#text");
     let boxes = document.querySelector("#boxes");
+    let reset = document.querySelector("#reset");
+    let start = document.querySelector("#start");
+    let count = 0;
 
     for(let i=0;i<=40;i++){
         let div= document.createElement("div");
         div.classList.add("box");
         boxes.append(div);
         div.innerText = arr[i];
+        
+        div.addEventListener("click", () => {
+            count++;
+            card.value = card.value+div.innerText;
+            document.querySelector("#hit0").innerText = count;
+        })
     }
 
-    
-    let boxs = document.querySelectorAll(".box");
-    let count = 0;
-
-    boxs.forEach((box) => {
-        box.setAttribute("contenteditable", "true")
-
-            box.addEventListener("focus" , (event) => {
-                card.innerText = card.innerText + box.innerText;
-                count++;
-                document.querySelector("#hit0").innerText = count;  
-            })        
+    reset.addEventListener("click", () => {
+        location.reload();
     })
+
+    start.addEventListener("click",() => {
+        setTimeout(()=>{
+            alert("TIme Out!!");
+            card.value = "";
+            
+        },3000);
+    })
+
 })
