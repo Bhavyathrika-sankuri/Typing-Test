5d11102f33bb191fec55e0de99bbe8cdbf98b613
Bhavyathrika <Bhavyathrika@gmail.com>
2025-01-18 12:39:11 +0530

    updated temp conversion

--- tyt.js
+++ tyt.js
@@ -1,7 +1,18 @@
 
 document.addEventListener("DOMContentLoaded" , () => {
 
+    const arr = [1,2,3,4,5,6,7,8,9,0,"q","W","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","c","v","b","n","m","<",">","?","!",":","{","}"];
     let card = document.querySelector("#card");
+    let boxes = document.querySelector("#boxes");
+
+    for(let i=0;i<=40;i++){
+        let div= document.createElement("div");
+        div.classList.add("box");
+        boxes.append(div);
+        div.innerText = arr[i];
+    }
+
+    
     let boxs = document.querySelectorAll(".box");
     let count = 0;
 
