71a01de235316cf34b0bcb251236d1e7666b5959
Bhavyathrika <Bhavyathrika@gmail.com>
2025-01-18 12:09:08 +0530

    init commit

--- tyt.js
+++ tyt.js
@@ -3,18 +3,15 @@ document.addEventListener("DOMContentLoaded" , () => {
 
     let card = document.querySelector("#card");
     let boxs = document.querySelectorAll(".box");
-    
+    let count = 0;
+
     boxs.forEach((box) => {
         box.setAttribute("contenteditable", "true")
-        box.addEventListener("click" , () => {
-            box.focus();
 
-        })
-
-            box.addEventListener("keydown" , (event) => {
+            box.addEventListener("focus" , (event) => {
                 card.innerText = card.innerText + box.innerText;
-                console.log(event.key)
-            })
-        
+                count++;
+                document.querySelector("#hit0").innerText = count;  
+            })        
     })
 })
